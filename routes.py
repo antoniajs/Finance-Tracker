97d1adde525e70b7796d3f2990d171de52e174b4
ajs02 <antonia.jaimeson-sh@bison.howard.edu>
2025-10-01 18:32:48 -0400

    Added routes.py

---
+++ routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, request
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+from models import User
+
+auth_bp = Blueprint("auth", __name__)
